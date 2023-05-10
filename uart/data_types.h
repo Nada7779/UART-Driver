ec6de51c53b5e6b901d7e37dec456c17c336bfea
Nada7779 <118090905+Nada7779@users.noreply.github.com>
2023-05-11 02:15:31 +0300

    Add files via upload

---
+++ uart/data_types.h
@@ -0,0 +1,17 @@
+﻿
+#ifndef data_types_H_
+#define data_types_H_
+
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned long  int uint32_t;
+typedef signed char int8_t;
+typedef signed long  int int32_t;
+
+
+
+
+
+
+#endif /* data_TYPES_H_ */
